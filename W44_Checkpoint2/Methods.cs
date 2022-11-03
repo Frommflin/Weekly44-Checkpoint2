ecefe4c4fcfb9d7b8462e76f59db3ad8cec5c731
Frommflin <freakyfrommelin@outlook.com>
2022-11-03 10:40:17 +0100

    Added second class to handle methods, keeping program code cleaner

---
+++ W44_Checkpoint2/Methods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W44_Checkpoint2;
+
+/*  
+ *  --------------------------------------------
+ *  ---         Class for Level 2+           ---
+ *  --------------------------------------------
+*/
+
+Console.WriteLine("".PadRight(20) + "Total price: ".PadRight(20) + totalSum);
+namespace W44_Checkpoint2
+{
+    internal class Methods
+    {
+        public static List<Product> EnterItems()
+        {
+            string input;
+            List<Product> products = new List<Product>();
+
+            while (true)
+            {
+                string category = "";
+                string productName = "";
+                int price = 0;
+
+                Console.Write("Category: ");
+                input = Console.ReadLine();
+                category = input.Trim();
+                if (input.Trim().ToLower() == "q")
+                {
+                    break;
+                }
+
+                Console.Write("Product name: ");
+                input = Console.ReadLine();
+                productName = input.Trim();
+                if (input.Trim().ToLower() == "q")
+                {
+                    break;
+                }
+
+                Console.Write("Price: ");
+                input = Console.ReadLine();
+
+                if (input.Trim().ToLower() == "q")
+                {
+                    break;
+                }
+                else if (int.TryParse(input, out int value))
+                {
+                    price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Price has to be a number! Automatically set to 0");
+                    price = 0;
+                }
+
+                products.Add(new Product(category, productName, price));
+            }
+            return products;
+        }
+    }
+}
